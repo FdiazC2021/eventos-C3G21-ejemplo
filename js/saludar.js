19560239f90377fba0ade947df48679a456a8c19
Frank Diaz <pachodiaz@gmail.com>
2021-09-29 20:27:07 -0500

    fin de tailwind, inicio de JavaScript

---
+++ js/saludar.js
@@ -0,0 +1,17 @@
+// alert("Hola mundo segunda forma");
+
+
+console.log("Mensaje por consola");
+
+document.write("<h1>Mensaje HTML</h1>");
+
+var nombre = "Juan";
+let correo = "pachodiaz@gmail.com";
+const mayor = 18;
+let edad = 28;
+let altura = 1.84;
+let = extranjero = true;
+
+let mensaje = "Mi nombre es "+nombre+", mi correo es "+correo;
+
+document.write("<h2>"+mensaje+"</h2>");
